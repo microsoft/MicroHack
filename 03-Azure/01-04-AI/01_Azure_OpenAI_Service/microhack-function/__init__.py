cca7a0fb08a1316508d594d7290c93aa47d00117
baudischrobin <robin.baudisch@statworx.com>
2023-03-27 11:48:58 +0200

    Retrieve Key Vault secrets, form recognizer capabilities to function.

--- 03-Azure/01-04-AI/01_Azure_OpenAI_Service/microhack-function/__init__.py
+++ 03-Azure/01-04-AI/01_Azure_OpenAI_Service/microhack-function/__init__.py
@@ -1,9 +1,86 @@
 import logging
-
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from azure.core.credentials import AzureKeyCredential
 import azure.functions as func
+
+
+def analyze_layout(data, endpoint, key):
+    document_analysis_client = DocumentAnalysisClient(
+        endpoint=endpoint, credential=AzureKeyCredential(key)
+    )
+
+    poller = document_analysis_client.begin_analyze_document(
+            "prebuilt-layout", data)
+    result = poller.result()
+
+    for idx, style in enumerate(result.styles):
+        print(
+            "Document contains {} content".format(
+                "handwritten" if style.is_handwritten else "no handwritten"
+            )
+        )
+
+    for page in result.pages:
+        print("""----Analyzing layout from page #{}----"""
+              .format(page.page_number))
+        print(
+            "Page has width: {} and height: {}, measured with unit: {}".format(
+                page.width, page.height, page.unit
+            )
+        )
+
+        for line_idx, line in enumerate(page.lines):
+            words = line.get_words()
+            print(
+                """...Line # {} has word count {} and text '{}'"""
+                .format(
+                    line_idx,
+                    len(words),
+                    line.content
+                )
+            )
+
+    for table_idx, table in enumerate(result.tables):
+        print(
+            "Table # {} has {} rows and {} columns".format(
+                table_idx, table.row_count, table.column_count
+            )
+        )
+        for region in table.bounding_regions:
+            print(
+                "Table # {} on page: {}".format(
+                    table_idx,
+                    region.page_number
+                )
+            )
+        for cell in table.cells:
+            print(
+                "...Cell[{}][{}] has content '{}'".format(
+                    cell.row_index,
+                    cell.column_index,
+                    cell.content,
+                )
+            )
+
+    print("----------------------------------------")
 
 
 def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
+
+    # Azure Credentials
+    credential = DefaultAzureCredential()
+    # This is the call to the Form Recognizer endpoint
+    key_vault_name = "microhack-key-vault"
+    key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
+    client = SecretClient(vault_url=key_vault_uri, credential=credential)
+    logging.info("Retreiving secrets from Azure Key Vault.")
+    apim_key = client.get_secret("FORM-RECOGNIZER-KEY").value
+    endpoint = client.get_secret("FORM-RECOGNIZER-ENDPOINT").value
+    logging.info("ENDPOINT: {}".format(endpoint))
+    data = myblob.read()
+    analyze_layout(data, endpoint, apim_key)

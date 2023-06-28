7fd4473b17e8e3796b866daae25eb8fbc5c5565a
Manuel Kutscher <kutscherma@outlook.com>
2023-06-28 15:03:44 +0200

    Fix bastion host

--- 03-Azure/01-03-Infrastructure/04_Azure_Monitor/prerequisites/terraform/bastion.tf
+++ 03-Azure/01-03-Infrastructure/04_Azure_Monitor/prerequisites/terraform/bastion.tf
@@ -4,6 +4,8 @@ resource "azurerm_public_ip" "pip_bastion" {
   resource_group_name = var.rg_name
   allocation_method   = "Static"
   sku                 = "Standard"
+
+  depends_on = [ azurerm_resource_group.microhack_monitoring ]
 }
 
 resource "azurerm_bastion_host" "bastion" {
@@ -13,12 +15,12 @@ resource "azurerm_bastion_host" "bastion" {
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = var.subnet_id
+    subnet_id            = azurerm_subnet.microhack_subnet_bastion.id
     public_ip_address_id = azurerm_public_ip.pip_bastion.id
   }
 }
 
-resource "azurerm_subnet" "microhack_subnet" {
+resource "azurerm_subnet" "microhack_subnet_bastion" {
   name                  = "AzureBastionSubnet"
   address_prefixes      = ["10.0.4.0/26"]
   virtual_network_name  = azurerm_virtual_network.microhack_vnet.name  

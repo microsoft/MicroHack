0bc5d050435303b3b7f9d01dc9f919d5a0409649
Dane Dahlem <dane@dahlem.it>
2024-01-25 12:49:05 +0100

    bump dependencies swap vuneral gulp with babel fix errors format code

--- 03-Azure/01-01-App Innovation/03_ServerlessMicroservices/Flightbooker/flightbooker-frontend/src/store/index.js
+++ 03-Azure/01-01-App Innovation/03_ServerlessMicroservices/Flightbooker/flightbooker-frontend/src/store/index.js
@@ -1,67 +1,63 @@
-import { createStore } from 'vuex';
-import {getAllSeats, bookSeat} from "@/services/BookingService";
+import { bookSeat, getAllSeats } from "@/services/BookingService";
+import { createStore } from "vuex";
 
 // Create a new store instance.
-export const store= createStore({
+export const store = createStore({
   state: {
-      selected: [],
-      selectedCount: 0,
-      seatList: [],
-
+    selected: [],
+    selectedCount: 0,
+    seatList: [],
   },
   mutations: {
-    SET_SEATS(state, seats){
-      state.seatList= seats
+    SET_SEATS(state, seats) {
+      state.seatList = seats;
     },
-    BOOK_SEATS(state,seats){
-      state.seatList= seats
+    BOOK_SEATS(state, seats) {
+      state.seatList = seats;
     },
-    ADD(state, payload){
-      state.selected=payload;
+    ADD(state, payload) {
+      state.selected = payload;
     },
-    REMOVE(state,payload){
-      state.selected=payload;
+    REMOVE(state, payload) {
+      state.selected = payload;
     },
-    COUNT(state, payload){
+    COUNT(state, payload) {
       state.selectedCount += payload;
     },
-    RESET_COUNT(state){
+    RESET_COUNT(state) {
       state.selectedCount = 0;
-    }
+    },
   },
   actions: {
-    getSeats({commit}){
-      getAllSeats()
-      .then(seats => {
-        commit('SET_SEATS', seats)
-      })
+    getSeats({ commit }) {
+      getAllSeats().then((seats) => {
+        commit("SET_SEATS", seats);
+      });
     },
-    bookSeats({commit}, payload){
-      bookSeat({payload})
-      .then(seats => {
-        commit('BOOK_SEATS', seats)
-      })
+    bookSeats({ commit }, payload) {
+      bookSeat({ payload }).then((seats) => {
+        commit("BOOK_SEATS", seats);
+      });
     },
-    add(context, payload){
-      const selected = context.state.selected
-      selected.push(payload)
-      context.commit('ADD',selected)
+    add(context, payload) {
+      const selected = context.state.selected;
+      selected.push(payload);
+      context.commit("ADD", selected);
     },
-    remove(context, payload){
-      const selected = context.state.selected
-      for( var i=0; i < selected.length; i++){
-        if ( selected[i] == payload){
-          selected.splice(i,1);
+    remove(context, payload) {
+      const selected = context.state.selected;
+      for (var i = 0; i < selected.length; i++) {
+        if (selected[i] == payload) {
+          selected.splice(i, 1);
         }
       }
-      context.commit('REMOVE',selected)
+      context.commit("REMOVE", selected);
     },
-    count(context, payload){
-      context.commit('COUNT', payload);
+    count(context, payload) {
+      context.commit("COUNT", payload);
     },
-    resetCount(context){
-      context.commit('RESET_COUNT')
+    resetCount(context) {
+      context.commit("RESET_COUNT");
     },
-    
-  }
-})
+  },
+});

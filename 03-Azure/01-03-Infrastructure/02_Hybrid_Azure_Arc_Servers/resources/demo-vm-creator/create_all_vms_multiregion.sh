1b258232230815887c69c1366338c2b7cbaea55a
TheFitzZZ <fitzzz@fitzzz.de>
2024-02-19 09:56:34 +0100

    multi-region deployment

---
+++ 03-Azure/01-03-Infrastructure/02_Hybrid_Azure_Arc_Servers/resources/demo-vm-creator/create_all_vms_multiregion.sh
@@ -0,0 +1,141 @@
+az group create --name "rg-onpremvms" --location "swedencentral"
+
+
+# Loop to create 5 Windows VMs in GWC
+for i in {1..5}
+do
+    # Define unique names for resource group and VM
+    resourceGroupLocation="germanywestcentral"
+    adminUsername="MHAdmin"
+    adminPassword="REPLACEME"
+    myResourceGroup="rg-onpremvms"
+    
+    resourceGroupName="rg-onpremvms"
+    vmName="vm-win-mh$i"
+    networkInterfaceName="vm-win-mh$i"
+    publicIpAddressName="pip-win-mh$i"
+    networkSecurityGroupName="nsg-win-mh$i"
+    virtualNetworkName="vnet-win-mh$i"
+    virtualMachineComputerName=vmName
+    virtualMachineRG=resourceGroupName
+    virtualMachineSize="Standard_D2ads_v5"
+    deploymentName="MHWinDeploy$1"
+    # Define parameters
+
+
+    # Create a resource group
+    # az group create --name $resourceGroupName --location $resourceGroupLocation
+
+    # Create a VM
+    az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file ./template-win.json \
+    --parameters @parameters-win.json \
+    --parameters virtualMachineName=$vmName adminUsername=$adminUsername adminPassword=$adminPassword networkInterfaceName=$networkInterfaceName publicIpAddressName=$publicIpAddressName networkSecurityGroupName=$networkSecurityGroupName virtualNetworkName=$virtualNetworkName virtualMachineComputerName=$virtualMachineComputerName virtualMachineRG=$virtualMachineRG virtualMachineSize=$virtualMachineSize location=$resourceGroupLocation
+done
+
+# Loop to create 5 Windows VMs in NEU
+for i in {6..10}
+do
+   
+    # Define unique names for resource group and VM
+    resourceGroupLocation="northeurope"
+    adminUsername="MHAdmin"
+    adminPassword="REPLACEME"
+    myResourceGroup="rg-onpremvms"
+    
+    resourceGroupName="rg-onpremvms"
+    vmName="vm-win-mh$i"
+    networkInterfaceName="vm-win-mh$i"
+    publicIpAddressName="pip-win-mh$i"
+    networkSecurityGroupName="nsg-win-mh$i"
+    virtualNetworkName="vnet-win-mh$i"
+    virtualMachineComputerName=vmName
+    virtualMachineRG=resourceGroupName
+    virtualMachineSize="Standard_D2ads_v5"
+    deploymentName="MHWinDeploy$1"
+    # Define parameters
+
+
+    # Create a resource group
+    # az group create --name $resourceGroupName --location $resourceGroupLocation
+
+    # Create a VM
+    az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file ./template-win.json \
+    --parameters @parameters-win.json \
+    --parameters virtualMachineName=$vmName adminUsername=$adminUsername adminPassword=$adminPassword networkInterfaceName=$networkInterfaceName publicIpAddressName=$publicIpAddressName networkSecurityGroupName=$networkSecurityGroupName virtualNetworkName=$virtualNetworkName virtualMachineComputerName=$virtualMachineComputerName virtualMachineRG=$virtualMachineRG virtualMachineSize=$virtualMachineSize location=$resourceGroupLocation
+done
+
+
+# Loop to create 5 Linux VMs in Sweden Central
+for i in {1..5}
+do
+    # Define unique names for resource group and VM
+    resourceGroupLocation="swedencentral"
+    adminUsername="MHAdmin"
+    adminPassword="REPLACEME"
+    myResourceGroup="rg-onpremmachines"
+    
+    resourceGroupName="rg-onpremvms"
+    vmName="vm-lnx-mh$i"
+    networkInterfaceName="vm-lnx-mh$i"
+    publicIpAddressName="pip-lnx-mh$i"
+    networkSecurityGroupName="nsg-lnx-mh$i"
+    virtualNetworkName="vnet-lnx-mh$i"
+    virtualMachineComputerName=vmName
+    virtualMachineRG=resourceGroupName
+    virtualMachineSize="Standard_D2ads_v5"
+    deploymentName="MHLnxDeploy$1"
+    # Define parameters
+
+
+    # Create a resource group
+    # az group create --name $resourceGroupName --location $resourceGroupLocation
+
+    # Create a VM
+    az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file ./template-lnx.json \
+    --parameters @parameters-lnx.json \
+    --parameters virtualMachineName=$vmName adminUsername=$adminUsername adminPassword=$adminPassword networkInterfaceName=$networkInterfaceName publicIpAddressName=$publicIpAddressName networkSecurityGroupName=$networkSecurityGroupName virtualNetworkName=$virtualNetworkName virtualMachineComputerName=$virtualMachineComputerName virtualMachineRG=$virtualMachineRG virtualMachineSize=$virtualMachineSize location=$resourceGroupLocation
+done
+
+
+# Loop to create 5 Linux VMs in France Central
+for i in {6..10}
+do
+    # Define unique names for resource group and VM
+    resourceGroupLocation="francecentral"
+    adminUsername="MHAdmin"
+    adminPassword="REPLACEME"
+    myResourceGroup="rg-onpremmachines"
+    
+    resourceGroupName="rg-onpremvms"
+    vmName="vm-lnx-mh$i"
+    networkInterfaceName="vm-lnx-mh$i"
+    publicIpAddressName="pip-lnx-mh$i"
+    networkSecurityGroupName="nsg-lnx-mh$i"
+    virtualNetworkName="vnet-lnx-mh$i"
+    virtualMachineComputerName=vmName
+    virtualMachineRG=resourceGroupName
+    virtualMachineSize="Standard_D2ads_v5"
+    deploymentName="MHLnxDeploy$1"
+    # Define parameters
+
+
+    # Create a resource group
+    # az group create --name $resourceGroupName --location $resourceGroupLocation
+
+    # Create a VM
+    az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file ./template-lnx.json \
+    --parameters @parameters-lnx.json \
+    --parameters virtualMachineName=$vmName adminUsername=$adminUsername adminPassword=$adminPassword networkInterfaceName=$networkInterfaceName publicIpAddressName=$publicIpAddressName networkSecurityGroupName=$networkSecurityGroupName virtualNetworkName=$virtualNetworkName virtualMachineComputerName=$virtualMachineComputerName virtualMachineRG=$virtualMachineRG virtualMachineSize=$virtualMachineSize location=$resourceGroupLocation
+done

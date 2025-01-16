098b8e019b22caa3e2bcb0f660c127d6bf071e4b
Anders Jansson <anjansso@microsoft.com>
2025-01-16 19:46:57 +0100

    Add files via upload

--- 03-Azure/01-03-Infrastructure/06_Migration_Datacenter_Modernization/resources/migration.ps1
+++ 03-Azure/01-03-Infrastructure/06_Migration_Datacenter_Modernization/resources/migration.ps1
@@ -1,14 +1,15 @@
 # Setting Variables
-$downloadpath = "https://aka.ms/unifiedinstaller"
+$Unifiedinstallerpath = "https://aka.ms/unifiedinstaller"
+$mysqlserverpath = "https://aka.ms/mysqlserverdownload"
 $outputpath = "C:\temp"
 
 
 # Create output directory
 New-Item -ItemType Directory -Path $outputpath
-
+New-Item -ItemType Directory -Path "$outputpath\ASRSetup"
 # Download files from repository
-Invoke-WebRequest -Uri $downloadpath -OutFile "$outputpath\AzureMigrateInstaller.zip"
-
+Invoke-WebRequest -Uri $Unifiedinstallerpath -OutFile "$outputpath\unifiedinstaller.exe"
+Invoke-WebRequest -Uri $mysqlserverpath -OutFile "$outputpath\ASRSetup\mysql-installer-community-5.7.20.0.msi"
 
 
 

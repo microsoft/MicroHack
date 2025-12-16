e50154c0037bc27ebcb14c2daca5bd633f6ed58b
Marco Weber <marcoweber@microsoft.com>
2025-12-16 12:49:45 +0100

    removed second edition

--- 03-Azure/01-03-Infrastructure/04_BCDR_Azure_Native_SE/Infra/App1/MODULES/VIRTUALMACHINE/VMEXTENSIONS/wrapperScript.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [string]$adminUsername,
-    [securestring]$adminPassword
-)
-
-$securePassword = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($adminUsername, $securePassword)
-
-Start-Process -FilePath 'powershell.exe' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File C:\path\to\myscript.ps1' -Credential $credential
